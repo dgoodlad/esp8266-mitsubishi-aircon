38623dbf6bba1bbf83fe0f140a0bc1ce068dc2d5
David Goodlad <david@goodlad.net>
2018-12-16 22:53:57 +1100

    Add a script to monitor and hexdump heatpump uart

---
+++ bin/monitor_heatpump.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Enable raw mode (don't do linux's TTY processing)
+stty -F /dev/ttyUSB0 raw
+
+# Disable echo when sending data
+stty -F /dev/ttyUSB0 -echo -echoe -echok
+
+# 2400 baud 8E1
+stty -F /dev/ttyUSB0 2400 cs8 -cstopb parenb -parodd
+
+od -x1 < /dev/ttyUSB0
